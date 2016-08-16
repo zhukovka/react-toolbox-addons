38a380b55b69eac02458c34f1facab825228aa96
zhukovka <helen.zhukova@gmail.com>
2016-08-16 20:55:43 +0300

    types

--- components/bottomsheet/index.d.ts
+++ components/bottomsheet/index.d.ts
@@ -1,4 +1,4 @@
-import __ReactToolbox from "../index.d.ts";
+import __ReactToolboxAddons from "../index.d.ts";
 
 export interface BottomSheetTheme {
   /**
@@ -28,47 +28,27 @@ export interface BottomSheetTheme {
   /**
    * Used as the className for the root element of the component.
    */
-  snackbar?: string;
+  bottomsheet?: string;
   /**
    * Added to the root element in case it's warning type.
    */
   warning?: string;
 }
 
-interface BottomSheetProps extends __ReactToolbox.Props {
+interface BottomSheetProps extends __ReactToolboxAddons.Props {
   /**
-   * Label for the action component inside the BottomSheet.
-   */
-  action?: string;
-  /**
-   * If true, the snackbar will be active.
+   * If true, the bottomsheet will be active.
    * @default true
    */
   active?: boolean;
   /**
-   * String key for an icon or Element which will be displayed in left side of the snackbar.
+   * String key for an icon or Element which will be displayed in left side of the bottomsheet.
    */
-  icon?: __React.ReactNode | string;
-  /**
-   * Text to display in the content. Required.
-   */
-  label: string;
-  /**
-   * Callback function when finish the set timeout.
-   */
-  onTimeout?: Function;
+  icon?: string;
   /**
    * Classnames object defining the component style.
    */
   theme?: BottomSheetTheme;
-  /**
-   * Amount of time in milliseconds after the BottomSheet will be automatically hidden.
-   */
-  timeout?: number;
-  /**
-   * Indicates the action type. Can be accept, warning or cancel
-   */
-  type?: "accept" | "cancel" | "warning";
 }
 
 export class BottomSheet extends __React.Component<BottomSheetProps, {}> { }
