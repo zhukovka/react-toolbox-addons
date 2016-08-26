0365c57b0af9453e735a7d0e51231b2d527d13a6
zhukovka <helen.zhukova@gmail.com>
2016-08-26 17:11:27 +0300

    кщще

--- spec/root.js
+++ spec/root.js
@@ -19,7 +19,7 @@ import StepsList from './components/stepslist';
 import {StatusComponent} from '../components/status-component';
 import CardTitleButtons from '../components/card-addons/CardTitleButtons';
 
-function checkButtonsActions(e){
+function checkButtonsActions (e) {
     console.log(e);
 }
 const testButtons = [
@@ -46,7 +46,7 @@ const Root = () => (
             <small>Spec {VERSION}</small>
         </h1>
         <CardTitlePrimary>
-                KUKURUKU
+            KUKURUKU
             <StatusComponent status="idle"/>
             <StatusComponent status="warning"/>
             <StatusComponent status="error"/>
@@ -54,14 +54,16 @@ const Root = () => (
             <CardTitleButtons buttons={testButtons}/>
 
         </CardTitlePrimary>
-                <CardTitle title='Default cardTitle' subtitle='def' style={{position: 'relative'}}>
-                    <CardTitleButtons buttons={testButtons}/>
-                </CardTitle>
-        <CardTitle title='Accent cardTitle' subtitle='def' style={{position: 'relative', background: 'rgb(255,64,129)', color: '#fff'}}>
+        <CardTitle title='Default cardTitle' subtitle='def' style={{position: 'relative'}}>
+            <CardTitleButtons buttons={testButtons}/>
+        </CardTitle>
+        <CardTitle title='Accent cardTitle' subtitle='def'
+                   style={{position: 'relative', background: 'rgb(255,64,129)', color: '#fff'}}>
             <CardTitleButtons buttons={testButtons}/>
         </CardTitle>
 
-        <NavigationChips headers={['some new text and', 'some new text', 'some new text', 'some new', 'some new']} currentIndex={2} />
+        <NavigationChips headers={['some new text and', 'some new text', 'some new text', 'some new', 'some new']}
+                         currentIndex={2}/>
 
         <section>
             <h5>StepsList</h5>
@@ -69,39 +71,52 @@ const Root = () => (
         </section>
 
         <BottomSheet />
-        <h3>CardActions</h3>
-        <CardActions>
-            <Button label="Action default 1"/>
-            <Button label="Action 2"/>
-        </CardActions>
-        <h3>CardActionsSpaced</h3>
-        <CardActionsRight>
-            <Button label="Action right1"/>
-            <Button label="Action 2"/>
-        </CardActionsRight>
-        <h3>CardActionsSpaced</h3>
-        <CardActionsSpaced>
-            <Button label="Left"/>
-            <ButtonGroup>
-                <Button label="right1"/>
-                <Button label="right2"/>
-            </ButtonGroup>
-        </CardActionsSpaced>
 
-        <Row expanded align={'middle'}>
-            <Col small={2}>
-                <Button label='ololosha' flat onClick={()=> {
+        <section>
+            <h5>CardActions</h5>
+            <CardActions>
+                <Button label="Action default 1"/>
+                <Button label="Action 2"/>
+            </CardActions>
+        </section>
+
+        <section>
+            <h5>CardActionsSpaced</h5>
+            <CardActionsRight>
+                <Button label="Action right1"/>
+                <Button label="Action 2"/>
+            </CardActionsRight></section>
+
+        <section>
+
+            <h5>CardActionsSpaced</h5>
+            <CardActionsSpaced>
+                <Button label="Left"/>
+                <ButtonGroup>
+                    <Button label="right1"/>
+                    <Button label="right2"/>
+                </ButtonGroup>
+            </CardActionsSpaced>
+        </section>
+
+        <section>
+            <h5>Grid</h5>
+            <Row expanded align={'middle'}>
+                <Col small={2}>
+                    <Button label='ololosha' flat onClick={()=> {
               console.log('kuku');
             }}/>
-            </Col>
-            <Col small={4} offset={'small-offset-6'} align={'stretch'}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam cumque explicabo ipsam, magni
-                molestias quidem quis saepe sit. Ab ad assumenda delectus et excepturi facere illo incidunt laborum,
-                sequi veniam?
-                <Button label='kukuruku' flat/>
-                <Button label='pysch' flat primary/>
-            </Col>
-        </Row>
+                </Col>
+                <Col small={4} offset={'small-offset-6'} align={'stretch'}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam cumque explicabo ipsam, magni
+                    molestias quidem quis saepe sit. Ab ad assumenda delectus et excepturi facere illo incidunt laborum,
+                    sequi veniam?
+                    <Button label='kukuruku' flat/>
+                    <Button label='pysch' flat primary/>
+                </Col>
+            </Row>
+
+        </section>
 
         <DropdownPlus/>
 
