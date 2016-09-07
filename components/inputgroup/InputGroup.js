7e16e109403c96f03eae3b1ce1282a542a464658
zhukovka <helen.zhukova@gmail.com>
2016-09-07 17:33:23 +0300

    rightIcon input group

--- components/inputgroup/InputGroup.js
+++ components/inputgroup/InputGroup.js
@@ -3,17 +3,24 @@ import classnames from 'classnames';
 import {themr} from 'react-css-themr';
 import {INPUTGROUP} from '../identifiers';
 
-const InputGroup = ({children, className, shadow, theme, ...other}) => {
+const InputGroup = ({children, className, shadow, rightIcon, theme, ...other}) => {
     const classes = classnames(theme.inputgroup, {
+        [theme.rightIcon]: rightIcon,
         [theme.shadow]: shadow
     }, className);
-    return (<div className={classes} {...other}>
-        {children}
-    </div>);
+    return (
+        <div className={classes} {...other}>
+            {children}
+        </div>);
 };
 InputGroup.propTypes = {
     children: PropTypes.any,
     className: PropTypes.string,
+    /**
+     * set to place any font icon to the right inside the preceding input
+     * Boolean rightIcon
+     */
+    rightIcon: PropTypes.bool,
     /**
      * set to add bottom shadow to the input group
      * Boolean shadow
@@ -21,6 +28,7 @@ InputGroup.propTypes = {
     shadow: PropTypes.bool,
     theme: PropTypes.shape({
         inputgroup: PropTypes.string,
+        rightIcon: PropTypes.string,
         shadow: PropTypes.string
     })
 };
