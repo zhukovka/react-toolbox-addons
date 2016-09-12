da35ffe4d57caf52e5fbe4bcb41dbaae53f5ebcb
zhukovka <helen.zhukova@gmail.com>
2016-09-12 20:58:01 +0300

    Avatar checkbox

--- components/avatarcheckbox/AvatarCheckbox.js
+++ components/avatarcheckbox/AvatarCheckbox.js
@@ -4,58 +4,29 @@ import Checkbox from 'react-toolbox/lib/checkbox';
 import {themr} from 'react-css-themr';
 import {AVATARCHECKBOX} from '../identifiers';
 
-const factory = (props) => {
+const factory = () => {
     class AvatarCheckbox extends Checkbox {
-        state = {checked: false};
 
         static propTypes = {
-            avatar: PropTypes.string,
-            checked: PropTypes.bool,
-            className: PropTypes.string,
-            disabled: PropTypes.bool,
-            label: PropTypes.oneOfType([
-                PropTypes.string,
-                PropTypes.node
-            ]),
-            name: PropTypes.string,
-            onChange: PropTypes.func,
-            theme: PropTypes.shape({
-                disabled: PropTypes.string,
-                field: PropTypes.string,
-                input: PropTypes.string,
-                ripple: PropTypes.string
-            })
-        };
-
-        onButtonClick = function () {
-            this.state.checked = !this.state.checked;
-            this.setState(this.state);
-            if (this.props.onChange) {
-                this.props.onChange(this.state.checked);
-            }
+            avatar: PropTypes.string
         };
 
         render () {
-            if (this.state.checked) {
-                return (
-                    <div style={{width: '56px', height: '56px'}}>
-                        <Button
-                            icon='check'
-                            floating
-                            accent
-                            onClick={this.onButtonClick.bind(this)}
-                        />
-                    </div>
-                );
+            const btnProps = {
+                floating: true,
+                onClick: ()=>this.props.onChange(!this.props.checked)
+            };
+
+            if (this.props.checked) {
+                btnProps.accent = true;
+                btnProps.icon = 'check';
+            } else {
+                btnProps.style = {backgroundImage: 'url(' + this.props.avatar + ')', backgroundSize: 'cover'};
             }
 
             return (
                 <div style={{width: '56px', height: '56px'}}>
-                    <Button
-                        style={{backgroundImage: 'url(' + this.props.avatar + ')', backgroundSize: 'cover'}}
-                        floating
-                        onClick={this.onButtonClick.bind(this)}
-                    />
+                    <Button {...btnProps}/>
                 </div>
             );
         }
