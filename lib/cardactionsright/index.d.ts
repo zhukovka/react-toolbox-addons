4974ed1799338e3889afdeaad10336c448829476
Mitya Kononov <mytcat@gmail.com>
2016-11-17 18:23:26 +0200

    wip

---
+++ lib/cardactionsright/index.d.ts
@@ -0,0 +1,10 @@
+import __ReactToolbox from "../index.d.ts";
+
+interface CardActionsProps extends __ReactToolbox.Props {
+  /**
+   * Children to pass through the component.
+   */
+  children?: __React.ReactNode;
+}
+
+export class CardActions extends __React.Component<CardActionsProps, {}> { }
