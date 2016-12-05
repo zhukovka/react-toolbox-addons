0ae970b73f1c129339c7187f20297ba4e7161319
Taras <tskrsaiuk@gmail.com>
2016-12-05 12:52:04 +0200

    del style prop in RedHeading, just put new style to theme.scss

--- components/headings/RedHeading.js
+++ components/headings/RedHeading.js
@@ -4,10 +4,10 @@ import classnames from 'classnames';
 import {SIDEBAR_PLUS} from '../identifiers';
 
 const factory = () => {
-    const RedHeading = ({children, className, theme, style})=> {
+    const RedHeading = ({children, className, theme})=> {
         const classes = classnames(theme.redHeading, className);
         return (
-            <h5 className={classes} style={style}>
+            <h5 className={classes}>
                 {children}
             </h5>
         );
@@ -16,7 +16,6 @@ const factory = () => {
     RedHeading.propTypes = {
         children: PropTypes.any,
         className: PropTypes.string,
-        style: PropTypes.object,
         theme: PropTypes.shape({
             redHeading: PropTypes.string
         })
