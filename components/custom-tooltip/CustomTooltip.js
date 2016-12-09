e49500c3c6a1100cfe282ac7d930eacbcb9538cd
Taras <tskrsaiuk@gmail.com>
2016-12-09 19:50:20 +0200

    in CustomTooltip, remove theme from props

--- components/custom-tooltip/CustomTooltip.js
+++ components/custom-tooltip/CustomTooltip.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Tooltip from 'react-toolbox/lib/tooltip';
 
-const CustomTooltip = Tooltip(props => <div {...props} />);
+const CustomTooltip = Tooltip(({theme, ...props}) => <div {...props} />);
 
 export default CustomTooltip;
