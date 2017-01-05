8c57e4be11ff2ddc0fb7c55c6d2bd47644fdf07e
Taras <tskrsaiuk@gmail.com>
2017-01-05 18:03:29 +0200

    update UI for SidebarPlus

--- components/sidebarplus/SidebarPlus.js
+++ components/sidebarplus/SidebarPlus.js
@@ -3,6 +3,8 @@ import {themr} from 'react-css-themr';
 import classnames from 'classnames';
 import {SIDEBAR_PLUS} from '../identifiers';
 import {Sidebar, AppBar} from 'react-toolbox';
+import {FontIcon} from 'react-toolbox';
+import {IconButton} from 'react-toolbox';
 
 const factory = () => {
     class SidebarPlus extends Component {
@@ -11,6 +13,8 @@ const factory = () => {
             className: PropTypes.string,
             pinned: PropTypes.bool,
             scrollY: PropTypes.bool,
+            headerAction: PropTypes.func,
+            headerActionIcon: PropTypes.string,
             theme: PropTypes.shape({
                 pinned: PropTypes.string,
                 scrollY: PropTypes.string,
@@ -18,6 +22,7 @@ const factory = () => {
                 sidebarContent: PropTypes.string
             }),
             title: PropTypes.string,
+            titleIcon: PropTypes.string,
             width: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 25, 33, 50, 66, 75, 100])
         };
 
@@ -26,14 +31,22 @@ const factory = () => {
         }
 
         render () {
-            const {title, children, theme, pinned, className} = this.props;
+            const {title, titleIcon, headerActionIcon, headerAction, children, theme, pinned, className} = this.props;
             const classes = classnames(theme.sidebarPlus, className);
             return (
                 <Sidebar pinned={pinned} width={5} theme={theme} className={classes}>
                     <AppBar theme={theme}>
-                        <h5>
-                            {title}
-                        </h5>
+                        <div className={theme.appBarHeader}>
+                            <div className={theme.title}>
+                                <h5>
+                                    {title}
+                                </h5>
+                                <FontIcon value={titleIcon}/>
+                            </div>
+                            <IconButton icon={headerActionIcon} onClick={(e)=> headerAction()}/>
+                        </div>
+
+
                     </AppBar>
                     {children}
                 </Sidebar>
