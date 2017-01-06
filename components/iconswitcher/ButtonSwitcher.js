d46e1762f0f06ed71996c2ace49f0d6144f7dd02
Taras <tskrsaiuk@gmail.com>
2017-01-06 18:08:08 +0200

    update style for ButtonSwitcher

--- components/iconswitcher/ButtonSwitcher.js
+++ components/iconswitcher/ButtonSwitcher.js
@@ -4,12 +4,18 @@ import {themr} from 'react-css-themr';
 import {BUTTON_SWITCHER} from '../identifiers';
 import {Button} from 'react-toolbox/lib/button';
 import {IconSwitcher} from './IconSwitcher.js';
+import {FontIcon} from 'react-toolbox/lib/font_icon';
 
 const ButtonSwitcher = ({active, icons, labels, className, theme, onSwitch, ...other}) => {
     const renderIcons = ()=> {
         return icons.map((icon, i)=> {
             const classname = classnames(theme.button, {[theme.active]: icon === active});
-            return (<Button theme={theme} icon={icon} label={labels[i]} key={`${icon}-${i}`} onClick={()=>onSwitch(icon)} className={classname}/>);
+            return (
+                <Button theme={theme} key={`${icon}-${i}`} onClick={()=>onSwitch(icon)} className={classname}>
+                    {labels[i]}
+                    <FontIcon value={icon}/>
+                </Button>
+            );
 
         });
     };
