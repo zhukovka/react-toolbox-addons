10cd98a2365a7fb9718dbaf4edee779eddddcbfd
Taras <tskrsaiuk@gmail.com>
2017-02-01 14:20:18 +0200

    restore styles css for Alert

--- components/custom-toastr/Toastr.js
+++ components/custom-toastr/Toastr.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import Alert from 'react-s-alert';
-
+import '../../node_modules/react-s-alert/dist/s-alert-default.css';
+import '../../node_modules/react-s-alert/dist/s-alert-css-effects/slide.css';
+import '../../node_modules/react-s-alert/dist/s-alert-css-effects/scale.css';
+import '../../node_modules/react-s-alert/dist/s-alert-css-effects/bouncyflip.css';
+import '../../node_modules/react-s-alert/dist/s-alert-css-effects/flip.css';
+import '../../node_modules/react-s-alert/dist/s-alert-css-effects/genie.css';
+import '../../node_modules/react-s-alert/dist/s-alert-css-effects/jelly.css';
+import '../../node_modules/react-s-alert/dist/s-alert-css-effects/stackslide.css';
 
 const Toastr = (props) => {
   return (<Alert {...props}/>);
