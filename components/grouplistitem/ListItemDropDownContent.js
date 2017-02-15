93770dd056cc446780e49bde151d8b1f9332012f
taraskrasiuk <tskrasiuk@gmail.com>
2017-02-15 11:06:36 +0200

    updt listItemDropDownContent

--- components/grouplistitem/ListItemDropDownContent.js
+++ components/grouplistitem/ListItemDropDownContent.js
@@ -2,11 +2,11 @@ import React, {PropTypes} from 'react';
 import {themr} from 'react-css-themr';
 import Dropdown from 'react-toolbox/lib/dropdown';
 import {LIST_ITEM_DROP_DOWN_CONTENT} from '../identifiers.js';
-const ListItemDropDownContent = ({title, theme, source, ...other}) => {
+const ListItemDropDownContent = ({title, theme, source, selected, ...other}) => {
   return (
     <div className={theme.listItemContent}>
         <span className={theme.listItemContent_title}>{title}</span>
-        <Dropdown className={theme.listItemContent_dropDown} value={source[0].value} source={source} {...other}/>
+        <Dropdown className={theme.listItemContent_dropDown} value={selected} source={source} {...other}/>
     </div>
   );
 };
@@ -15,7 +15,8 @@ ListItemDropDownContent.propTypes = {
     onChange: PropTypes.func,
     source: PropTypes.array,
     theme: PropTypes.object,
-    title: PropTypes.string
+    title: PropTypes.string,
+    selected: PropTypes.object
 };
 const factory = () => ListItemDropDownContent;
 export default themr(LIST_ITEM_DROP_DOWN_CONTENT)(ListItemDropDownContent);
