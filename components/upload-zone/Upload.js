00aea82fa8b5af572c83656ff11ada5b3924aa8f
taraskrasiuk <tskrasiuk@gmail.com>
2017-04-04 12:58:28 +0300

    check for Upload, check for images types and for req ( if exist) width and height of image

--- components/upload-zone/Upload.js
+++ components/upload-zone/Upload.js
@@ -4,9 +4,13 @@ import {UPLOAD} from '../identifiers';
 import {UploadButton} from '../upload-button';
 import ProgressBar from 'react-toolbox/lib/progress_bar';
 import {
-    CSS_UPLOAD_ACTIVE
+    CSS_UPLOAD_ACTIVE,
+    FILE_TYPE_JPEG,
+    FILE_TYPE_JPG,
+    FILE_TYPE_PNG,
+    ERROR_BAD_TYPE,
+    ERROR_REQUIREMENTS
 } from './constants';
-
 
 class Upload extends Component {
     static propTypes = {
@@ -16,26 +20,33 @@ class Upload extends Component {
         defaultClass: PropTypes.string,
         onUpload: PropTypes.func,
         theme: PropTypes.object,
-        imageUrl: PropTypes.string
+        imageUrl: PropTypes.string,
+        requirements: PropTypes.objectOf({
+            width: PropTypes.number,
+            height: PropTypes.number
+        })
     };
     static defaultProps = {
         defaultClass: UPLOAD
     };
-    constructor (props) {
+
+    constructor(props) {
         super(props);
         this.state = {
             isDragActive: false,
             imageUrl: props.imageUrl,
-            progress: 0
+            progress: 0,
+            error: null
         };
     }
-    onDragLeave () {
+
+    onDragLeave() {
         this.setState({
             isDragActive: false
         });
     }
 
-    onDragOver (e) {
+    onDragOver(e) {
         e.preventDefault();
         e.dataTransfer.dropEffect = 'copy';
         this.setState({
@@ -43,17 +54,18 @@ class Upload extends Component {
         });
     }
 
-    onUpload (e) {
-        const types = ["jpg", "jpeg", "png"];
+    onUpload(e) {
         const checkType = (file) => {
-            var split = file.name.split(".");
-            const type = split[split.length - 1].toLowerCase();
-            return types.indexOf(type) != -1;
+            if (file != null) {
+                return [FILE_TYPE_JPG, FILE_TYPE_JPEG, FILE_TYPE_PNG].indexOf(file.type) != -1;
+            }
+            return false;
         };
+
         let file;
         e.preventDefault();
-        const errorHandler = (evt)=>{
-            switch (evt.target.error.code){
+        const errorHandler = (evt)=> {
+            switch (evt.target.error.code) {
                 case evt.target.error.NOT_FOUND_ERR:
                     console.log('File Not Found!');
                     break;
@@ -78,57 +90,98 @@ class Upload extends Component {
             }
         };
 
-        const reader = new FileReader();
-        reader.onerror = errorHandler;
-        reader.onloadstart = ()=>{
-            this.setState({
-                progress: 1
-            });
-        };
-        reader.onload = () => {
-            this.setState({
-                progress: 100
-            });
-        };
-        reader.onprogress = updateProgress;
-        reader.onloadend = () => {
-            this.setState({
-                progress: 0,
-                imageUrl: reader.result,
-                isDragActive: false
-            }, ()=>{
-                this.props.onUpload(file, reader.result, e);
-            });
-        };
-
         if (e.dataTransfer) {
             file = e.dataTransfer.files[0];
         } else {
             file = e.target.files[0];
         }
 
-        reader.readAsDataURL(file);
-    }
-
-    renderContent () {
-        const {progress, imageUrl} = this.state;
-        if (!progress) {
-            return (
-               <UploadButton icon="photo_camera"
-                           imageUrl={imageUrl}
-                           onUpload={this.onUpload.bind(this)}
-               />
-            );
+        if (checkType(file)) {
+            const self = this;
+            const reader = new FileReader();
+            reader.onerror = errorHandler;
+            reader.onloadstart = ()=> {
+                this.setState({
+                    progress: 1
+                });
+            };
+            reader.onload = () => {
+                this.setState({
+                    progress: 100
+                });
+            };
+            reader.onprogress = updateProgress;
+            reader.onloadend = () => {
+                const img = new Image();
+                img.src = window.URL.createObjectURL(file);
+                img.onload = () => {
+                    window.URL.revokeObjectURL(img.src);
+                    const w = img.naturalWidth;
+                    const h = img.naturalHeight;
+                    const {requirements} = self.props;
+                    if (requirements != null && (w > requirements.width && h >requirements.height)) {
+                        reader.abort();
+                        self.setState({
+                            error: ERROR_REQUIREMENTS,
+                            progress: 0
+                        });
+                    } else {
+                        self.setState({
+                            progress: 0,
+                            imageUrl: reader.result,
+                            isDragActive: false,
+                            error: null
+                        }, ()=> {
+                            self.props.onUpload(file, reader.result, e);
+                        });
+                    }
+                };
+            };
+            reader.readAsDataURL(file);
         } else {
-            return (
-                <div style={{padding: '10.8rem'}}>
-                    <ProgressBar value={this.state.progress} mode='determinate'/>
-                </div>
-            );
+            this.setState({
+                error: ERROR_BAD_TYPE
+            });
         }
     }
 
-    render () {
+    getUploadErrorMessage(msgType) {
+        const {requirements} = this.props;
+        let message;
+        switch (msgType) {
+            case ERROR_REQUIREMENTS :
+                message = "Image should have width " + requirements.width + " and height " + requirements.height;
+                break;
+            case ERROR_BAD_TYPE :
+                message = "Image should have extension one of " + [FILE_TYPE_JPEG, FILE_TYPE_JPG, FILE_TYPE_PNG].map(str => str.split("/")[1]).join(", ") + ".";
+                break;
+        }
+        return message;
+    }
+
+    renderContent() {
+        const {progress, imageUrl, error} = this.state;
+        const {requirements, theme} = this.props;
+        return (
+            <div className={theme.uploadWrapper}>
+                <UploadButton icon="photo_camera"
+                              imageUrl={imageUrl}
+                              onUpload={this.onUpload.bind(this)}
+                />
+                {progress && !error ? (
+                    <div style={{padding: '10.8rem'}}>
+                        <ProgressBar value={this.state.progress} mode='determinate'/>
+                    </div>
+                ) : !progress && requirements != null && error ? (
+                    <span className={theme.errorMessage}>
+                        {this.getUploadErrorMessage(error)}
+                    </span>
+                ) : null}
+            </div>
+        );
+    }
+
+    render() {
         const {theme, className, activeClass, defaultClass} = this.props;
         const {isDragActive} = this.state;
         let classes = classnames(theme[defaultClass], {
--- spec/components/TestUpload.js
+++ spec/components/TestUpload.js
@@ -4,7 +4,9 @@ import {Upload} from '../../components/upload-zone';
 
 const TestUpload = () => {
     return (
-        <Upload onUpload={(file)=>{console.log(file);}}/>
+        <Upload onUpload={(file)=>{console.log(file);}}
+            requirements={{width: 1200, height: 800}}
+        />
     );
 };
 
