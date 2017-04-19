5643871fab0fbc3d89d61bf6c595e057a68db5d1
taraskrasiuk <tskrasiuk@gmail.com>
2017-04-19 14:37:21 +0300

    delete tooltip from grid-item

--- components/grid-item/GridItem.js
+++ components/grid-item/GridItem.js
@@ -1,21 +1,18 @@
 import React, {PropTypes} from 'react';
 import classnames from 'classnames';
 import Avatar from 'react-toolbox/lib/avatar';
-import CustomTooltip from '../custom-tooltip';
 
 const GridItem = ({icon, title, theme, selected, master, ...other}) => {
     const styleWrapper = classnames(theme.gridItem, {
         [theme.selected]: selected
     });
     return (
-        <CustomTooltip tooltip={title} label={title} tooltipPosition={'top'}>
-            <div {...other} className={styleWrapper}>
-                <Avatar className={classnames(theme.avatar, {
-                    [theme.master]: master
-                })} icon={icon}/>
-                <span className={theme.title}>{title}</span>
-            </div>
-        </CustomTooltip>
+        <div {...other} className={styleWrapper}>
+            <Avatar className={classnames(theme.avatar, {
+                [theme.master]: master
+            })} icon={icon}/>
+            <span className={theme.title}>{title}</span>
+        </div>
     );
 };
 
